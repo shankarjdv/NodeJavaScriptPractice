388d7375ae0c2771994adfbf258b773244f8c2b4
unknown <shankarjadhav4177@gmail.com>
2023-12-02 16:37:38 +0530

    removed express un-related code

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-// const http = require("http");
-// const fs = require("fs");
-// const url = require("url");
-
 const express = require("express");
-const app = express(); // app is like handler function
+const app = express(); // app is like handler function now
 
 app.get("/",(req,res)=>{
     return res.send("Hello from Home page")
@@ -15,34 +11,3 @@ app.get("/about",(req,res)=>{
 
 app.listen(8000,() => console.log("Server Started!"))
 
-
-
-// function myHandler(req,res){
-//     if(req.url === "/favicon.ico") return res.end();
-//     const myurl = url.parse(req.url,true);
-//     const log = `${Date.now()}: ${req.url} new Request received\n`
-//     console.log("myurl==>",myurl)
-//     // non blocking operation
-//     fs.appendFile("log.txt",log,(err,data)=>{
-
-//         switch(myurl.pathname) { 
-//             case "/":
-//                 res.end("Home Page");
-//                 break;
-//             case "/contactMe":
-//                 res.end("contact Me");
-//                 break;
-//             case "/about":
-//                 res.end("I am Shankar Jadhav");
-//                 break;
-//             default :
-//                 res.end("Hello From Server Again");
-
-//         }
-//     })
-// }
-
-
-// const myServer = http.createServer(app);
-
-// myServer.listen(8000,()=> console.log("server started!"));

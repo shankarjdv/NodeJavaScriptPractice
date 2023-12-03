c2cfa8810a3d1e43440496c56e3759b0bbc0956d
unknown <shankarjadhav4177@gmail.com>
2023-12-03 11:17:26 +0530

    POST API to insert data into json file

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json")
+const fs = require('fs')
 
 const app = express(); // app is like handler function now
 const PORT = 8000;
+
+//middlaware - plugin
+app.use(express.urlencoded({extended: false}));
 
 app.get("/api/users",(req,res)=>{
     return res.json(users);
@@ -15,6 +19,37 @@ app.get("/users",(req,res)=>{
     `
     return res.send(html)
 });
+
+app
+.route("/api/users/:id")
+.get((req,res)=>{
+    const id = Number(req.params.id);
+    const user =users.find((user)=>user.id === id);
+    return res.json(user);
+   
+})
+.put((req,res)=>{
+    // edit with id
+    return res.json({status: "pending"});
+})
+.patch((req,res)=>{
+    return res.json({status: "pending"});
+})
+.delete((req,res)=>{
+    return res.json({status: "pending"});
+});
+
+
+app.post("/api/users",(req,res)=>{
+    const data = req.body;
+    console.log("data====>",data)
+    users.push({...data,id: users.length+1});
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+        return res.json({status: "success", id: users.length});
+    })
+
+    // return res.json({status: "pending"});
+})
 
 app.get("/about",(req,res)=>{
     return res.send("Hello from About page")
